de15c0b76a76f7e3cc3d94147df3df9e2f9f5389
Raul de Melo <melo.raulf@gmail.com>
2019-06-25 22:54:33 +0200

    fix: remove useless node version

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  release: {
-    branch: "master"
-  },
+  // release: {
+  //   branch: "master"
+  // },
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
