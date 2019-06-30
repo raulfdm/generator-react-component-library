efb0f69abb14a3b33e9aae782e9bda39907a3762
Raul de Melo <melo.raulf@gmail.com>
2019-06-30 23:21:55 +0200

    test: implement test to check src folder

--- generators/app/index.test.js
+++ generators/app/index.test.js
@@ -2,6 +2,11 @@ const helpers = require('yeoman-test');
 const path = require('path');
 const assert = require('yeoman-assert');
 const rimraf = require('rimraf');
+const { promisify } = require('util');
+const fs = require('fs-extra');
+const glob = require('glob');
+
+const promiseGlob = promisify(glob);
 
 const { ROOT_FILES } = require('./resources/constants');
 
@@ -26,7 +31,32 @@ describe('React Component Library Generator', () => {
 
     it('copy all root files file', () => {
       const files = ROOT_FILES.map(generateDistPath);
+
       assert.file(files);
+    });
+
+    it('copy all files from "src" folder', async () => {
+      const templatesFile = path.join(__dirname, './templates/src/**/*');
+
+      const files = await promiseGlob(templatesFile);
+      const rootFilesInTmp = files
+        .filter(file => fs.lstatSync(file).isFile())
+        .map(file => {
+          /* TODO: This is too hacky 🤦🏽‍, refactor pls */
+          const splittedFilePath = file.split('/');
+          const srcIndex = splittedFilePath.findIndex(
+            pathPart => pathPart === 'src',
+          );
+
+          const filePath = splittedFilePath
+            .slice(srcIndex, splittedFilePath.length)
+            .join('/');
+
+          return filePath;
+        })
+        .map(fileName => path.join(TMP_PATH, fileName));
+
+      assert.file(rootFilesInTmp);
     });
   });
 });
