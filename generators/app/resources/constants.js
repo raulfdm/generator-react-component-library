aba9b745b447fa3c2c6e8cb6729ac408de46af1f
Raul de Melo <melo.raulf@gmail.com>
2019-06-30 22:46:34 +0200

    chore: move constants inside resources folder

---
+++ generators/app/resources/constants.js
@@ -0,0 +1,17 @@
+const YARN_CHOICE = 'yarn';
+const NPM_CHOICE = 'npm';
+
+/* TODO: implement reading folder way to provide them */
+const ROOT_FILES = [
+  '.editorconfig',
+  '.eslintrc.js',
+  '.gitignore',
+  'rollup.config.js',
+  'babel.config.js',
+];
+
+module.exports = {
+  NPM_CHOICE,
+  YARN_CHOICE,
+  ROOT_FILES,
+};
