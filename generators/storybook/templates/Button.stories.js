29b0172471d56a8dc4cfcdd250d9b42a04398094
Raul Melo <melo.raulf@gmail.com>
2020-04-12 20:00:46 +0200

    refactor: change story model to CSF

--- generators/storybook/templates/Button.stories.js
+++ generators/storybook/templates/Button.stories.js
@@ -1,21 +1,25 @@
+/* How to write stories: https://storybook.js.org/docs/formats/component-story-format/ */
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { withReadme } from 'storybook-readme';
 import { action } from '@storybook/addon-actions';
 
 import README from './README.md';
 import Button from './Button';
 
-const stories = storiesOf('Button', module).addDecorator(withReadme(README));
+export default {
+  title: 'Button',
+  component: Button,
+  decorators: [withReadme(README)],
+};
 
-stories.add('Default', () => (
+export const basic = () => (
   <Button onClick={action('clicked')}>Hello Button</Button>
-));
+);
 
-stories.add('With some emoji', () => (
+export const passingChildren = () => (
   <Button onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
   </Button>
-));
+);
